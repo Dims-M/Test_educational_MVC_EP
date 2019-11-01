7bd5d1f21ee05c05ed18a83f69746aea6057afc3
Дмитрий <rabolan@mail.ru>
2019-11-02 01:56:25 +0300

    добавлена возможность покупки книги + форма_запись в бд

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using Test_educational_MVC_EP.Models;
 
+
+//https://www.youtube.com/watch?v=o5o2jW-o5PY&list=PLL-k0Ff5RfqXnwdDG61WqZ2j3KXUPnfmq&index=6&t=230s
 namespace Test_educational_MVC_EP.Controllers
 {
     /// <summary>
@@ -23,6 +25,34 @@ namespace Test_educational_MVC_EP.Controllers
             ViewBag.Books = books;
             return View();
 
+        }
+
+        /// <summary>
+        /// Покупка книги.
+        /// </summary>
+        /// <param name="id">Параметр ID нужной книги</param>
+        /// <returns>возращает вьюху</returns>
+        public ActionResult Buy(int? id)
+        {
+            ViewBag.BookId = id; //
+            return View();
+        }
+
+        /// <summary>
+        /// Метод Byu покупки с заполненым параметром
+        /// </summary>
+        /// <param name="purchase">заполненый параметр из вьюхи</param>
+        /// <returns></returns>
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            //Добавляем в БД инфу о покупке
+            bd.Purchases.Add(purchase);
+
+            bd.SaveChanges();
+
+           return $"Cпасибо {purchase.Person}, за покупку книги";
         }
 
         public ActionResult About()
